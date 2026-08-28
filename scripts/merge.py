27cc407e5f7655fe77af2d89acbb36ee39bc459e
paulduchesne <paulduchesne@tuta.io>
2026-08-29 08:44:02 +1000

    added merge script for json input

--- scripts/merge.py
+++ scripts/merge.py
@@ -1,50 +1,94 @@
 
-# merge entities.
+# merge entities based on overlap data input.
 
+import json
 import pandas
 import pathlib
+import tqdm
 import uuid
 
-# input uris to be merged.
+def merge_auth(d):
 
-input_uris = [
-    'https://dev.fiafcore.org/684e617a-ae04-4070-a6d9-885aa2565706',
-    'https://dev.fiafcore.org/804b6fcd-a05a-4223-b91b-af23e88a8a28'
-]
+    # load input id, and minimum validation on uri.
 
-# output uri, which will replace all inputs.
-# note this does not have to be a **new** identifier, but this seems like a good, agnostic, choice.
-#
-# something to consider for the future is logging identifier changes.
+    input_uris = d['id']
+    for i in input_uris:
+        if 'dev.fiafcore.org' not in i:
+            raise Exception(f'{i} not well formed.')
 
-output_uri = f'https://dev.fiafcore.org/{str(uuid.uuid4())}'
+    # output uri, which will replace all inputs.
+    # note this does not have to be a **new** identifier, but this seems like a good, agnostic, choice.
+    #
+    # something to consider for the future is logging identifier changes.
 
-print(f'minted identifier: {output_uri}')
+    output_uri = f'https://dev.fiafcore.org/{str(uuid.uuid4())}'
 
-# auth files for processing.
+    # auth files for processing.
 
-auth_files = [
-    pathlib.Path.cwd().parent / 'transform' / 'bfi' / 'auth.parquet',
-    pathlib.Path.cwd().parent / 'transform' / 'bundesarchiv' / 'auth.parquet'
-]
+    auth_files = [
+        pathlib.Path.cwd().parent / 'transform' / 'bfi' / 'auth.parquet',
+        pathlib.Path.cwd().parent / 'transform' / 'bundesarchiv' / 'auth.parquet'
+    ]
 
-# verify auth files exist.
+    # verify auth files exist.
 
-for a in auth_files:
-    if not a.exists():
-        raise Exception(f'{a} not found.')
+    for a in auth_files:
+        if not a.exists():
+            raise Exception(f'{a} not found.')
 
-# find and replace.
+    # find and replace, if relevant.
 
-for a in auth_files:
-    df = pandas.read_parquet(a)
-    df.loc[df.fiafcore.isin(input_uris), 'fiafcore'] = output_uri
-    df.to_parquet(a, compression="zstd")
+    for a in auth_files:
+        df = pandas.read_parquet(a)
+        match = df.loc[df.fiafcore.isin(input_uris)]
+        if not len(match):
+            continue
 
-# report on result.
+        df.loc[df.fiafcore.isin(input_uris), 'fiafcore'] = output_uri
+        df.to_parquet(a, compression="zstd")
 
-for a in auth_files:
-    df = pandas.read_parquet(a)
-    print(a.parent.stem)
-    print('input:', len(df.loc[df.fiafcore.isin(input_uris)]))
-    print('output:', len(df.loc[df.fiafcore.isin([output_uri])]))
+def validate_auth(d):
+
+    # load input id, and minimum validation on uri.
+
+    input_uris = d['id']
+    for i in input_uris:
+        if 'dev.fiafcore.org' not in i:
+            raise Exception(f'{i} not well formed.')
+
+    # auth files for checking.
+
+    auth_files = [
+        pathlib.Path.cwd().parent / 'transform' / 'bfi' / 'auth.parquet',
+        pathlib.Path.cwd().parent / 'transform' / 'bundesarchiv' / 'auth.parquet'
+    ]
+
+    # verify auth files exist.
+
+    for a in auth_files:
+        if not a.exists():
+            raise Exception(f'{a} not found.')
+
+    # check if input uris are still present.
+
+    for a in auth_files:
+        df = pandas.read_parquet(a)
+        match = df.loc[df.fiafcore.isin(input_uris)]
+        if len(match):
+            raise Exception(f'{input_uris} detected in {a}.')
+
+# path to overlap file.
+
+data_path = pathlib.Path.home() / 'Desktop' / 'overlap.json'
+if not data_path.exists():
+    raise Exception(f'{data_path} not found.')
+
+# load data and process.
+
+data = json.loads(data_path.read_text())
+
+for x in tqdm.tqdm(data, desc='merge'):
+    merge_auth(x)
+
+for x in tqdm.tqdm(data, desc='validate'):
+    validate_auth(x)
